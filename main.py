255555719c1619badaf6233261b440adc3a930f2
imlinux68 <104739714+imlinux68@users.noreply.github.com>
2023-04-08 21:31:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def home():
     return "Hello World!!! <h1>HELO FROM FLASK!!!</h1>"
 
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show profile of current user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # shoe id with data, id - integer num
+    return 'Post %d' % post_id
+
 if __name__ == "__main__"
     app.run()
